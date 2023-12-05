435a1f9d95efbdabd82845e3f760d4f6bc39ce27
‘blockCloth’ <blockClothSword@gmail.com>
2023-12-05 14:48:57 +0800

    日志文件编写完成

--- coding-web/src/main/java/com/coding/blog/web/CodingBlogApplication.java
+++ coding-web/src/main/java/com/coding/blog/web/CodingBlogApplication.java
@@ -1,14 +1,20 @@
 package com.coding.blog.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  */
+@Slf4j
 @SpringBootApplication
 public class CodingBlogApplication {
     public static void main(String[] args) {
+        log.info("start coding-blog");
+        log.warn("start coding-blog warn");
+        log.error("start coding-blog error");
+        log.debug("start coding-blog debug");
         SpringApplication.run(CodingBlogApplication.class, args);
     }
 }
